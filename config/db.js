4ab609dd19f550545298c6faf39d030111f9ad09
Viraj Patidar <vrj381@gmail.com>
2022-04-01 11:52:53 +0530

    DB setup

---
+++ config/db.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_CONNECTION_URL);
+        console.log('Database connected 🥳');
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.log('Connection failed ☹️');
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const connectDB = require('./config/db')
+connectDB();
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
