9ab98bfb54be9ce91120136429bc2ba04e9d1ea0
Viraj Patidar <vrj381@gmail.com>
2022-04-02 13:23:09 +0530

    clean scheduler

--- routes/files.js
+++ routes/files.js
@@ -3,6 +3,8 @@ const multer = require('multer');
 const path = require('path');
 const File = require('../models/file');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
 
 
 let storage = multer.diskStorage({
@@ -20,7 +22,27 @@ let upload = multer({
 
 
 
+async function clean() {
+    const files = await File.find({ createdAt: { $lt: new Date(Date.now() - 24 * 60 * 60 * 1000) } });
+    if (files.length) {
+        for (const file of files) {
+            try {
+                fs.unlinkSync(file.path);
+                await file.remove();
+                console.log(`successfully deleted ${file.filename}`);
+            } catch (err) {
+                console.log(`error while deleting file ${err} `);
+            }
+        }
+    }
+    console.log('Job done!');
+}
+
+
+
 router.post('/', (req, res) => {
+
+    clean().then(process.exit);
 
     // Validate request + Store file
     upload(req, res, async (err) => {
